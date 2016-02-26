8ccc76dfdb27afe4534a68b68f723c8090034c9f
Terry Ott <Terry.Ott@pobox.com>
2016-02-25 22:19:37 -0500

    Building/running with ant

--- src/PlanetPbfUpdate.java
+++ src/PlanetPbfUpdate.java
@@ -5,6 +5,8 @@ import crosby.binary.file.*;
 import java.util.List;
 import java.io.*;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+
 public class PlanetPbfUpdate
 {
    	public static void main(String[] args)
@@ -30,11 +32,44 @@ public class PlanetPbfUpdate
 	public static class PbfParser implements BlockReaderAdapter
 	{
 		public boolean skipBlock(FileBlockPosition message) {
-			return true;
+			System.out.println("skipBlock called on " + message.getType());
+
+			if ( message.getType().equals("OSMHeader") )
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
 		}
 
 		public void handleBlock(FileBlock message)	{
-			;
+			System.out.println("Got a fileblock");
+
+			if ( message.getType().equals("OSMHeader") == true )
+			{
+				System.out.println("Got header in handleblock");
+
+				try
+				{
+					Osmformat.HeaderBlock headerBlock = 
+						Osmformat.HeaderBlock.parseFrom(message.getData());	
+
+					if ( headerBlock.hasOsmosisReplicationTimestamp() == true )
+					{
+
+					}
+					else
+					{
+						System.out.println("No timestamp");
+					}
+				} 
+				catch ( InvalidProtocolBufferException e )
+				{
+					System.out.println("Invalid protocol buffer");
+				}
+			}
 		}
 
         public void complete() {
