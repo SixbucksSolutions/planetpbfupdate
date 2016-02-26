e2a9545a99eb1c3cac420365950d1732efc453f3
Terry Ott <Terry.Ott@pobox.com>
2016-02-25 21:10:40 -0500

    ant building is going nicely

--- src/PlanetPbfUpdate.java
+++ src/PlanetPbfUpdate.java
@@ -3,78 +3,39 @@ import crosby.binary.Osmformat.*;
 import crosby.binary.file.*;
 
 import java.util.List;
+import java.io.*;
 
 public class PlanetPbfUpdate
 {
    	public static void main(String[] args)
 	{
-		System.out.println("Booya");
+		//System.out.println("Booya");
+		BlockReaderAdapter blockReader = new PbfParser();
+		InputStream input;
+		try
+		{
+			input = new FileInputStream("/home/tdo/tmp/pbf/monaco-latest.osm.pbf");
+			new BlockInputStream(input, blockReader).process();
+		} 
+		catch ( FileNotFoundException e )
+		{
+			System.out.println("File not found: " + e);
+		}
+		catch ( IOException e )
+		{
+			System.out.println("IO error when processing data");
+		}
 	}
 
-	private static class PbfParser extends BinaryParser 
+	public static class PbfParser implements BlockReaderAdapter
 	{
-        @Override
-        protected void parseRelations(List<Relation> rels) {
-			/*
-            if (!rels.isEmpty())
-                System.out.println("Got some relations to parse.");
-            Relation r = null;
-			*/
-        }
+		public boolean skipBlock(FileBlockPosition message) {
+			return true;
+		}
 
-        @Override
-        protected void parseDense(DenseNodes nodes) {
-			/*
-            long lastId=0;
-            long lastLat=0;
-            long lastLon=0;
-
-            for (int i=0 ; i<nodes.getIdCount() ; i++) {
-                lastId += nodes.getId(i);
-                lastLat += nodes.getLat(i);
-                lastLon += nodes.getLon(i);
-                System.out.printf("Dense node, ID %d @ %.6f,%.6f\n",
-                        lastId,parseLat(lastLat),parseLon(lastLon));
-            }
-			*/
-        }
-
-        @Override
-        protected void parseNodes(List<Node> nodes) {
-			/*
-            for (Node n : nodes) {
-                System.out.printf("Regular node, ID %d @ %.6f,%.6f\n",
-                        n.getId(),parseLat(n.getLat()),parseLon(n.getLon()));
-            }
-			*/
-        }
-
-        @Override
-        protected void parseWays(List<Way> ways) {
-			/*
-            for (Way w : ways) {
-                System.out.println("Way ID " + w.getId());
-                StringBuilder sb = new StringBuilder();
-                sb.append("  Nodes: ");
-                long lastRef = 0;
-                for (Long ref : w.getRefsList()) {
-                    lastRef+= ref;
-                    sb.append(lastRef).append(" ");
-                }
-                sb.append("\n  Key=value pairs: ");
-                for (int i=0 ; i<w.getKeysCount() ; i++) {
-                    sb.append(getStringById(w.getKeys(i))).append("=")
-                            .append(getStringById(w.getVals(i))).append(" ");
-                }
-                System.out.println(sb.toString());
-            }
-			*/
-        }
-
-        @Override
-        protected void parse(HeaderBlock header) {
-            System.out.println("Got header block.");
-        }
+		public void handleBlock(FileBlock message)	{
+			;
+		}
 
         public void complete() {
             System.out.println("Complete!");
